8628fdc04729f427dced3b4e2f89b829179f8a5c
Maria sol Carugo <69329898+SolCarugo@users.noreply.github.com>
2020-09-23 16:42:11 -0300

    Create mood.py

---
+++ mood.py
@@ -0,0 +1,8 @@
+mood= int(input("¿sos feliz?"))
+if mood == "si":
+    status = True
+    status == True
+    print("mantene la sonrisa siempre")
+else:
+    status = False
+    print("no te desanimes, todo va a mejorar")
